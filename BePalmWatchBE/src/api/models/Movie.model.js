8a85bc0143f63390293b1b7b224dab6ea1273dd8
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-10 18:32:03 +0200

    update movie model

--- BePalmWatchBE/src/api/models/Movie.model.js
+++ BePalmWatchBE/src/api/models/Movie.model.js
@@ -2,8 +2,13 @@ import mongoose from "mongoose";
 
 const MovieSchema = new mongoose.Schema(
   {
-    id: { type: String, required: true },
     title: { type: String, required: true },
+    poster_path: { type: String, required: false },
+    release_date: { type: String, required: true },
+    genres: [{ type: String, required: true }],
+    original_language: { type: String, required: true },
+    overview: { type: String, required: true },
+    homepage: { type: String, required: true },
     showtime: [{ type: mongoose.Schema.Types.ObjectId, ref: "showtime" }],
   },
   { timestamps: true, collection: "movies" }
