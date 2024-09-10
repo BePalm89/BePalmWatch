59a9b84994f753bec4dcf67370d17d6ddb8f3249
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-10 17:55:01 +0200

    add connection to db

--- BePalmWatchBE/index.js
+++ BePalmWatchBE/index.js
@@ -1,6 +1,12 @@
 import server from './src/config/server.js';
+import * as dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
+
+dotenv.config();
 
 const PORT = 3000;
+
+connectDB();
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
