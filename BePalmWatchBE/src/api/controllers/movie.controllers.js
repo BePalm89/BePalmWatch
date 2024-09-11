fa72ea0ffdcdf7adb6121de9c090bc7929a84d8b
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-11 11:59:16 +0200

    BE - add endpoint for get movie by id

--- BePalmWatchBE/src/api/controllers/movie.controllers.js
+++ BePalmWatchBE/src/api/controllers/movie.controllers.js
@@ -8,3 +8,14 @@ export const getAllMovies = async (req, res, next) => {
     return next(error);
   }
 };
+
+export const getMovieById = async (req, res, next) => {
+  try {
+    const { id } = req.params; 
+    const movie = await Movie.findById(id);
+    
+    return res.status(200).json(movie);
+  } catch (error) {
+    return next(error);
+  }
+}
--- BePalmWatchBE/src/api/routes/movie.routes.js
+++ BePalmWatchBE/src/api/routes/movie.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getAllMovies } from '../controllers/movie.controllers.js';
+import { getAllMovies, getMovieById } from '../controllers/movie.controllers.js';
 
 const router = express.Router();
 
 router.get('/', getAllMovies);
+router.get('/:id', getMovieById);
 
 export default router;
