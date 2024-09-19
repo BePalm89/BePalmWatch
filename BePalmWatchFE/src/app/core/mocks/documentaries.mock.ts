38191950ef23182ad50a1a77b7b9595b06543d9f
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 12:34:21 +0200

    add documentary service test

---
+++ BePalmWatchFE/src/app/core/mocks/documentaries.mock.ts
@@ -0,0 +1,22 @@
+export const DOCUMENTARY_MOCK = [
+    {
+      adult: false,
+      backdrop_path: "/rGiiJARZf0SkyhIqf4j0mQQdycI.jpg",
+      genre_ids: [
+        35,
+        99,
+        10770
+      ],
+      id: 1270880,
+      original_language: "en",
+      original_title: "Katt Williams: Woke Foke",
+      overview: "Comedian Katt Williams lets loose in real time as he hits the stage for Netflix's second livestreamed stand-up event.",
+      popularity: 61.833,
+      poster_path: "/hjgAEcu8JEpeF7KEtmjTzPFD3uL.jpg",
+      release_date: "2024-05-04",
+      title: "Katt Williams: Woke Foke",
+      video: false,
+      vote_average: 5.962,
+      vote_count: 13
+    }
+  ]
--- BePalmWatchFE/src/app/core/services/documentary.service.spec.ts
+++ BePalmWatchFE/src/app/core/services/documentary.service.spec.ts
@@ -1,16 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { DocumentaryService } from "./documentary.service";
+import { DOCUMENTARY_MOCK } from "../mocks/documentaries.mock";
 
-import { DocumentaryService } from './documentary.service';
-
-describe('DocumentaryService', () => {
+describe("DocumentaryService", () => {
   let service: DocumentaryService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DocumentaryService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should call getDocumentaries with the correct url, parameter and header and return an array of documentaries", () => {
+    const mockResponse = { results: DOCUMENTARY_MOCK };
+
+    service.getDocumentaries().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === "https://api.themoviedb.org/3/discover/movie" &&
+        request.method === "GET" &&
+        request.headers.get("Authorization") ===
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+        request.params.has("language") &&
+        request.params.get("language") === "en-US" &&
+        request.params.has("sort_by") &&
+        request.params.get("sort_by") === "popularity.desc" &&
+        request.params.has("with_genres") &&
+        request.params.get("with_genres") === "99"
+      );
+    });
+
+    req.flush(mockResponse);
   });
 });
