a77883770777f1002e37a9ab4196bf7db87e01d7
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 16:45:18 +0200

    add test for footer

--- BePalmWatchFE/src/app/core/layout/footer/footer.component.spec.ts
+++ BePalmWatchFE/src/app/core/layout/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -20,4 +21,23 @@ describe('FooterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the correct number of links in the template', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(component.FOOTER_LINKS.length);
+  });
+
+  it('should render links with correct href, icon', () => {
+    const linksElements = fixture.debugElement.queryAll(By.css('a'));
+
+    linksElements.forEach((linkEl, index) => {
+      const link = component.FOOTER_LINKS[index];
+      
+      const a = linkEl.nativeElement as HTMLAnchorElement;
+      expect(a.href).toEqual(link.url);
+
+      const img = linkEl.query(By.css('img'));
+      expect(img.nativeElement.src).toContain(link.icon);
+    })
+  })
 });
