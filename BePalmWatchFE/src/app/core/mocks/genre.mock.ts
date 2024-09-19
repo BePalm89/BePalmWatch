621b72d0c50ddf58feec64db16f274d11849a14c
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 13:26:31 +0200

    add test for genre service

---
+++ BePalmWatchFE/src/app/core/mocks/genre.mock.ts
@@ -0,0 +1,45 @@
+export const GENRE_MOVIE = {
+  genres: [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+  ],
+};
+
+export const GENRE_MOVIE_MAPPED = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+};
+
+export const GENRE_TV_SHOW = {
+  genres: [
+    {
+      id: 10759,
+      name: "Action & Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+  ],
+};
+
+export const GENRE_TV_SHOW_MAPPED = {
+  10759: 'Action & Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+}
--- BePalmWatchFE/src/app/core/services/genre.service.spec.ts
+++ BePalmWatchFE/src/app/core/services/genre.service.spec.ts
@@ -1,16 +1,118 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { GenreService } from './genre.service';
+import { GenreService } from "./genre.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { GENRE_MOVIE, GENRE_MOVIE_MAPPED, GENRE_TV_SHOW, GENRE_TV_SHOW_MAPPED } from "../mocks/genre.mock";
 
-describe('GenreService', () => {
+describe("GenreService", () => {
   let service: GenreService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(GenreService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  it("should call fetchGenreMovies with the right ulr, params and header and return the array of genre", () => {
+    const mockResponse = GENRE_MOVIE;
+
+    service.fetchGenreMovies().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === "https://api.themoviedb.org/3/genre/movie/list" &&
+          request.method === "GET",
+        request.headers.get("Authorization") ===
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+          request.params.has("language") &&
+          request.params.get("language") === "en-US"
+      );
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should set genre Movie correctly when using the setGenreMovie', () => {
+    // Given 
+    const genreData = GENRE_MOVIE;
+
+    console.log(genreData);
+
+    // When
+    service.setGenreMovie(genreData);
+
+    const genreMovieMap = service.getGenreMovie();
+
+    // Then
+    expect(genreMovieMap).toEqual(GENRE_MOVIE_MAPPED);
+  });
+
+  it('should return genreMapMovies as empty object initially', () => {
+    // When
+    const genreMapMovies = service.getGenreMovie();
+
+    // Then
+    expect(genreMapMovies).toEqual({});
+  });
+
+  it("should call fetchGenreTvShows with the right ulr, params and header and return the array of genre", () => {
+    const mockResponse = GENRE_TV_SHOW;
+
+    service.fetchGenreTvShows().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === "https://api.themoviedb.org/3/genre/tv/list" &&
+          request.method === "GET",
+        request.headers.get("Authorization") ===
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+          request.params.has("language") &&
+          request.params.get("language") === "en-US"
+      );
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should set genre TV correctly when using the setGenreTvShow', () => {
+    // Given 
+    const genreData = GENRE_TV_SHOW;
+
+    console.log(genreData);
+
+    // When
+    service.setGenreTvShow(genreData);
+
+    const genreMovieMap = service.getGenreTvShow();
+
+    // Then
+    expect(genreMovieMap).toEqual(GENRE_TV_SHOW_MAPPED);
+  });
+
+  it('should return genreMapMovies as empty object initially', () => {
+    // When
+    const genreMapTvShow = service.getGenreMovie();
+
+    // Then
+    expect(genreMapTvShow).toEqual({});
+  });
+
 });
