aa6075c15278c19d5576dfe7052bf653d9e644ec
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 15:39:49 +0200

    add movies service ttest

---
+++ BePalmWatchFE/src/app/core/mocks/movie.mock.ts
@@ -0,0 +1,213 @@
+export const MOVIE = {
+  page: 1,
+  results: [
+    {
+      adult: false,
+      backdrop_path: "/9l1eZiJHmhr5jIlthMdJN5WYoff.jpg",
+      genre_ids: [28, 35, 878],
+      id: 533535,
+      original_language: "en",
+      original_title: "Deadpool & Wolverine",
+      overview:
+        "A listless Wade Wilson toils away in civilian life with his days as the morally flexible mercenary, Deadpool, behind him. But when his homeworld faces an existential threat, Wade must reluctantly suit-up again with an even more reluctant Wolverine.",
+      popularity: 2176.871,
+      poster_path: "/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+      release_date: "2024-07-24",
+      title: "Deadpool & Wolverine",
+      video: false,
+      vote_average: 7.697,
+      vote_count: 2926,
+    },
+    {
+      adult: false,
+      backdrop_path: "/mKOBdgaEFguADkJhfFslY7TYxIh.jpg",
+      genre_ids: [28, 878, 35, 12, 53],
+      id: 365177,
+      original_language: "en",
+      original_title: "Borderlands",
+      overview:
+        "Returning to her home planet, an infamous bounty hunter forms an unexpected alliance with a team of unlikely heroes. Together, they battle monsters and dangerous bandits to protect a young girl who holds the key to unimaginable power.",
+      popularity: 1376.577,
+      poster_path: "/865DntZzOdX6rLMd405R0nFkLmL.jpg",
+      release_date: "2024-08-07",
+      title: "Borderlands",
+      video: false,
+      vote_average: 5.838,
+      vote_count: 521,
+    },
+    {
+      adult: false,
+      backdrop_path: "/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
+      genre_ids: [16, 10751, 12, 35, 14],
+      id: 1022789,
+      original_language: "en",
+      original_title: "Inside Out 2",
+      overview:
+        "Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
+      popularity: 1334.886,
+      poster_path: "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+      release_date: "2024-06-11",
+      title: "Inside Out 2",
+      video: false,
+      vote_average: 7.669,
+      vote_count: 3597,
+    },
+  ],
+  total_pages: 46091,
+  total_results: 921819,
+};
+
+export const FAVORITE_MOVIE = {
+    page: 1,
+    results: [
+      {
+        adult: false,
+        backdrop_path: "/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
+        genre_ids: [
+          16,
+          10751,
+          12,
+          35,
+          14
+        ],
+        id: 1022789,
+        original_language: "en",
+        original_title: "Inside Out 2",
+        overview: "Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
+        popularity: 1334.886,
+        poster_path: "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+        release_date: "2024-06-11",
+        title: "Inside Out 2",
+        video: false,
+        vote_average: 7.669,
+        vote_count: 3599
+      },
+      {
+        adult: false,
+        backdrop_path: "/lgkPzcOSnTvjeMnuFzozRO5HHw1.jpg",
+        genre_ids: [
+          16,
+          10751,
+          35,
+          28
+        ],
+        id: 519182,
+        original_language: "en",
+        original_title: "Despicable Me 4",
+        overview: "Gru and Lucy and their girls—Margo, Edith and Agnes—welcome a new member to the Gru family, Gru Jr., who is intent on tormenting his dad. Gru also faces a new nemesis in Maxime Le Mal and his femme fatale girlfriend Valentina, forcing the family to go on the run.",
+        popularity: 1106.233,
+        poster_path: "/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
+        release_date: "2024-06-20",
+        title: "Despicable Me 4",
+        video: false,
+        vote_average: 7.181,
+        vote_count: 1607
+      },
+      {
+        adult: false,
+        backdrop_path: "/mKOBdgaEFguADkJhfFslY7TYxIh.jpg",
+        genre_ids: [
+          28,
+          878,
+          35,
+          12,
+          53
+        ],
+        id: 365177,
+        original_language: "en",
+        original_title: "Borderlands",
+        overview: "Returning to her home planet, an infamous bounty hunter forms an unexpected alliance with a team of unlikely heroes. Together, they battle monsters and dangerous bandits to protect a young girl who holds the key to unimaginable power.",
+        popularity: 1376.577,
+        poster_path: "/865DntZzOdX6rLMd405R0nFkLmL.jpg",
+        release_date: "2024-08-07",
+        title: "Borderlands",
+        video: false,
+        vote_average: 5.838,
+        vote_count: 522
+      },
+
+    ],
+    total_pages: 1,
+    total_results: 5
+  }
+
+  export const MOVIE_DETAILS = {
+    adult: false,
+    backdrop_path: "/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
+    belongs_to_collection: {
+      id: 1022790,
+      name: "Inside Out Collection",
+      poster_path: "/kvJqs0vm29U0xJa373wlzHb3FRh.jpg",
+      backdrop_path: "/7U2m2dMSIfHx2gWXKq78Xj1weuH.jpg"
+    },
+    budget: 200000000,
+    genres: [
+      {
+        id: 16,
+        name: "Animation"
+      },
+      {
+        id: 10751,
+        name: "Family"
+      },
+      {
+        id: 12,
+        name: "Adventure"
+      },
+      {
+        id: 35,
+        name: "Comedy"
+      },
+      {
+        id: 14,
+        name: "Fantasy"
+      }
+    ],
+    homepage: "https://movies.disney.com/inside-out-2",
+    id: 1022789,
+    imdb_id: "tt22022452",
+    origin_country: [
+      "US"
+    ],
+    original_language: "en",
+    original_title: "Inside Out 2",
+    overview: "Teenager Riley's mind headquarters is undergoing a sudden demolition to make room for something entirely unexpected: new Emotions! Joy, Sadness, Anger, Fear and Disgust, who’ve long been running a successful operation by all accounts, aren’t sure how to feel when Anxiety shows up. And it looks like she’s not alone.",
+    popularity: 1334.886,
+    poster_path: "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
+    production_companies: [
+      {
+        id: 2,
+        logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+        name: "Walt Disney Pictures",
+        origin_country: "US"
+      },
+      {
+        id: 3,
+        logo_path: "/1TjvGVDMYsj6JBxOAkUHpPEwLf7.png",
+        name: "Pixar",
+        origin_country: "US"
+      }
+    ],
+    production_countries: [
+      {
+        iso_3166_1: "US",
+        name: "United States of America"
+      }
+    ],
+    release_date: "2024-06-11",
+    revenue: 1676778335,
+    runtime: 97,
+    spoken_languages: [
+      {
+        english_name: "English",
+        iso_639_1: "en",
+        name: "English"
+      }
+    ],
+    status: "Released",
+    tagline: "Make room for new emotions.",
+    title: "Inside Out 2",
+    video: false,
+    vote_average: 7.669,
+    vote_count: 3597
+  }
--- BePalmWatchFE/src/app/core/services/movies.service.spec.ts
+++ BePalmWatchFE/src/app/core/services/movies.service.spec.ts
@@ -1,16 +1,123 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MoviesService } from './movies.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FAVORITE_MOVIE, MOVIE, MOVIE_DETAILS } from '../mocks/movie.mock';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  let httpController: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(MoviesService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call getMovies with correct url, params and header and return an array of movies', () => {
+    const mockResponse = MOVIE;
+
+    service.getMovies().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === "https://api.themoviedb.org/3/discover/movie" &&
+        request.method === "GET",
+      request.headers.get("Authorization") ===
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+        request.params.has("language") &&
+        request.params.get("language") === "en-US" &&
+        request.params.has("sort_by") && 
+        request.params.get("sort_by") === 'popularity.desc'
+      );
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should call getFavoriteMovies with correct url, params and header and return an array of favorite movies', () => {
+    const mockResponse = FAVORITE_MOVIE;
+
+    service.getFavoriteMovies().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === "https://api.themoviedb.org/3/account/10346540/favorite/movies" &&
+        request.method === "GET",
+      request.headers.get("Authorization") ===
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+        request.params.has("language") &&
+        request.params.get("language") === "en-US" &&
+        request.params.has("sort_by") && 
+        request.params.get("sort_by") === 'popularity.desc'
+      );
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should call getDetailsMovie with correct url, params and header and return the details of a movies', () => {
+    const mockResponse = MOVIE_DETAILS;
+
+    service.getDetailsMovie(MOVIE_DETAILS.id).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === `https://api.themoviedb.org/3/movie/${MOVIE_DETAILS.id}` &&
+        request.method === "GET",
+      request.headers.get("Authorization") ===
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+        request.params.has("language") &&
+        request.params.get("language") === "en-US"
+      );
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should call addFavorite with correct url, params and header and return an successfull message ', () => {
+    const mockPayload = {
+      media_type: "movie",
+      media_id: 748783,
+      favorite: true
+    };
+
+    service.addFavorite(mockPayload).subscribe((response) => {
+      expect(response).toEqual({
+        success: true,
+        status_code: 1,
+        status_message: "Success."
+      });
+    });
+
+    const req = httpController.expectOne((request) => {
+      return (
+        request.url === `https://api.themoviedb.org/3/account/10346540/favoritE` &&
+        request.method === "POST",
+      request.headers.get("Authorization") ===
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo" &&
+        request.params.has("language") &&
+        request.params.get("language") === "en-US"
+      );
+    });
+
+    req.flush({});
+  });
+
 });
