f5537cf63eccbfe6d7e37dfcb52821a6942a1460
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 16:11:53 +0200

    add test for the now playing service

--- BePalmWatchFE/src/app/core/services/now-playing.service.spec.ts
+++ BePalmWatchFE/src/app/core/services/now-playing.service.spec.ts
@@ -1,16 +1,84 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { NowPlayingService } from './now-playing.service';
+import { NowPlayingService } from "./now-playing.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import {
+  NOW_PLAYING_MOVIE,
+  NOW_PLAYING_MOVIE_DETAILS,
+  NOW_PLAYING_MOVIE_DETAILS_UPDATED,
+  SHOWTIME_PAYLOAD,
+} from "../mocks/now-playing.mock";
 
-describe('NowPlayingService', () => {
+describe("NowPlayingService", () => {
   let service: NowPlayingService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(NowPlayingService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should call getNowPlayingMovies and return an array of movie", () => {
+    const mockResponse = NOW_PLAYING_MOVIE;
+
+    service.getNowPlayingMovies().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpController.expectOne({
+      method: "GET",
+      url: `http://localhost:3000/api/v1/movies`,
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it("should call getNowPlayingMovieById and return the details of a movie", () => {
+    const mockResponse = NOW_PLAYING_MOVIE_DETAILS;
+
+    service
+      .getNowPlayingMovieById(NOW_PLAYING_MOVIE_DETAILS._id)
+      .subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+    const req = httpController.expectOne({
+      method: "GET",
+      url: `http://localhost:3000/api/v1/movies/${NOW_PLAYING_MOVIE_DETAILS._id}`,
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it("should call updateNowPlayingMovie and return the details of a movie with the updated property", () => {
+    const mockPayload = SHOWTIME_PAYLOAD;
+    const mockResponse = NOW_PLAYING_MOVIE_DETAILS_UPDATED;
+
+    service
+      .updateNowPlayingMovie(NOW_PLAYING_MOVIE_DETAILS_UPDATED._id, mockPayload)
+      .subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+    const req = httpController.expectOne({
+      method: "PUT",
+      url: `http://localhost:3000/api/v1/movies/${NOW_PLAYING_MOVIE_DETAILS_UPDATED._id}`,
+    });
+
+    req.flush(mockResponse);
   });
 });
--- BePalmWatchFE/src/app/core/services/now-playing.service.ts
+++ BePalmWatchFE/src/app/core/services/now-playing.service.ts
@@ -7,9 +7,6 @@ import { Observable } from 'rxjs';
 })
 export class NowPlayingService {
 
-  private readonly TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZTU3YTA3NGM5MDM0Zjg0NmJhNTM4ODFhOWVmMWViNyIsIm5iZiI6MTcyNDE0MDA3Ny45MTUwODQsInN1YiI6IjYwNzA4ZGM4Y2MyNzdjMDAyYTgwMGE0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ck5in_IMa7ZGNC9cz-D1iXLfNDPoWb6MA-72x1ENSyo';
-  private readonly GET_MOVIE_NOW_PLAYING_MOVIEDB = 'https://api.themoviedb.org/3/movie/now_playing';
   private readonly GET_MOVIE_NOW_PLAYING = 'http://localhost:3000/api/v1/movies';
   private readonly GET_MOVIE_NOW_PLAYING_BY_ID = 'http://localhost:3000/api/v1/movies/';
   private readonly PUT_MOVIE_NOW_PLAYING_BY_ID = 'http://localhost:3000/api/v1/movies/';
