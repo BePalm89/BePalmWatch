829b9ba25877dc12d7b357c2914fd116dfda0199
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-19 17:26:21 +0200

    add test for top bar component

--- BePalmWatchFE/src/app/core/layout/top-bar/top-bar.component.spec.ts
+++ BePalmWatchFE/src/app/core/layout/top-bar/top-bar.component.spec.ts
@@ -1,23 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TopBarComponent } from './top-bar.component';
+import { TopBarComponent } from "./top-bar.component";
+import { provideRouter } from "@angular/router";
+import { By } from "@angular/platform-browser";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-describe('TopBarComponent', () => {
+describe("TopBarComponent", () => {
   let component: TopBarComponent;
   let fixture: ComponentFixture<TopBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TopBarComponent]
-    })
-    .compileComponents();
-    
+      imports: [TopBarComponent, NoopAnimationsModule],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TopBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render the correct number of links", () => {
+    const links = fixture.debugElement.queryAll(By.css("li"));
+    expect(links.length).toBe(component.LINKS.length);
   });
 });
