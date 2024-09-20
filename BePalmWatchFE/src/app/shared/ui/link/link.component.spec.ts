ebf54329429c7903d4b564afca4e0e91fe4d0478
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-20 12:44:56 +0200

    add test for the link component

--- BePalmWatchFE/src/app/shared/ui/link/link.component.spec.ts
+++ BePalmWatchFE/src/app/shared/ui/link/link.component.spec.ts
@@ -1,23 +1,69 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LinkComponent } from './link.component';
+import { LinkComponent } from "./link.component";
+import { Route, Router, provideRouter } from "@angular/router";
+import { Component } from "@angular/core";
 
-describe('LinkComponent', () => {
+@Component({
+  standalone: true,
+  template: "",
+})
+export class MockComponent {}
+
+describe("LinkComponent", () => {
   let component: LinkComponent;
   let fixture: ComponentFixture<LinkComponent>;
+  let routes: Route[];
+  let router: Router;
 
+  routes = [{ path: "tv", component: MockComponent }];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LinkComponent]
-    })
-    .compileComponents();
-    
+      imports: [LinkComponent],
+      providers: [provideRouter(routes)],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LinkComponent);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render link label, link url and icon correctly", () => {
+    // Given
+    component.linkLabel = "label";
+    component.linkUrl = "/link";
+    component.icon = "icon";
+
+    //When
+    fixture.detectChanges();
+
+    // Then
+    const linkElement = fixture.nativeElement.querySelector("a");
+    const imgElement = fixture.nativeElement.querySelector("img");
+
+    expect(linkElement).toBeTruthy();
+    expect(linkElement.textContent).toBe(component.linkLabel);
+    expect(linkElement.getAttribute("href")).toBe(component.linkUrl);
+
+    expect(imgElement).toBeTruthy();
+    expect(imgElement.getAttribute("src")).toBe(component.icon);
+    expect(imgElement.getAttribute("alt")).toBe(component.icon);
+  });
+
+  it("should have the active class when route link is active", async() => {
+    // Given 
+    const linkElement = fixture.nativeElement.querySelector("a");
+    // When 
+    await router.navigate([component.linkUrl]);
+
+    fixture.detectChanges();
+
+    // Then 
+    expect(linkElement.classList).toContain("active");
   });
 });
