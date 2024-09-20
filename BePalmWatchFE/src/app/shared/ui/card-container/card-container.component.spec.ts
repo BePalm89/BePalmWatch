d2e9b4407e0b5658fc8f2fcca32aef6c2eededa6
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-20 17:51:25 +0200

    add test for card container component

--- BePalmWatchFE/src/app/shared/ui/card-container/card-container.component.spec.ts
+++ BePalmWatchFE/src/app/shared/ui/card-container/card-container.component.spec.ts
@@ -1,23 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CardContainerComponent } from './card-container.component';
+import { CardContainerComponent } from "./card-container.component";
+import { Movie } from "../../../core/models/movie.model";
+import { MediaTypeEnum } from "../../../core/enum/media-type.enum";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { provideRouter } from "@angular/router";
+import { By } from "@angular/platform-browser";
 
-describe('CardContainerComponent', () => {
+describe("CardContainerComponent", () => {
   let component: CardContainerComponent;
   let fixture: ComponentFixture<CardContainerComponent>;
+  let httpController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardContainerComponent]
-    })
-    .compileComponents();
-    
+      imports: [CardContainerComponent, HttpClientTestingModule],
+      providers: [provideRouter([])],
+    }).compileComponents();
+
+    httpController = TestBed.inject(HttpTestingController);
     fixture = TestBed.createComponent(CardContainerComponent);
     component = fixture.componentInstance;
+    component.data = [
+      { id: 1, title: "movie 1" } as Movie,
+      { id: 2, title: "movie 2" } as Movie,
+    ];
+
+    component.type = MediaTypeEnum.MOVIE;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should render a card for each media item in data", () => {
+    const cardElement = fixture.nativeElement.querySelectorAll("app-card");
+    expect(cardElement.length).toBe(2);
+  });
+
+  it('should pass the correct data and type to app-card', () => {
+    const mediaItem = { title: 'Movie 1', id: 1 } as Movie;
+    component.data = [mediaItem];
+    component.type = MediaTypeEnum.MOVIE;
+    
+    fixture.detectChanges(); 
+    
+    const cardComponent = fixture.debugElement.queryAll(By.css('app-card'))[0].componentInstance;
+  
+    expect(cardComponent.data).toBe(mediaItem);
+    expect(cardComponent.type).toBe(MediaTypeEnum.MOVIE);
   });
 });
