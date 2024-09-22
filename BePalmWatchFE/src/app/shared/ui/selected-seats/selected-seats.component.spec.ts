4f36a2039cb3f5c2e574e55178db88deb6533a51
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-22 18:32:14 +0200

    add tests for seat legend and selected seat component

--- BePalmWatchFE/src/app/shared/ui/selected-seats/selected-seats.component.spec.ts
+++ BePalmWatchFE/src/app/shared/ui/selected-seats/selected-seats.component.spec.ts
@@ -1,23 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SelectedSeatsComponent } from './selected-seats.component';
+import { SelectedSeatsComponent } from "./selected-seats.component";
+import { By } from "@angular/platform-browser";
+import { SeatStatus } from "../../../core/enum/seat-status.enum";
 
-describe('SelectedSeatsComponent', () => {
+describe("SelectedSeatsComponent", () => {
   let component: SelectedSeatsComponent;
   let fixture: ComponentFixture<SelectedSeatsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SelectedSeatsComponent]
-    })
-    .compileComponents();
-    
+      imports: [SelectedSeatsComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SelectedSeatsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should display the right text when selected seat is an empty array", () => {
+    const emptySeatElement = fixture.debugElement.query(
+      By.css('[data-test="no-seats"')
+    ).nativeElement;
+
+    expect(emptySeatElement.textContent).toBe(
+      "Selection of seats ( maximum: 6 )"
+    );
+  });
+
+  it("should display the correct seats with the right row and seat number", () => {
+    component.selectedSeatsList = [
+      { row: 1, seat: 1, status: SeatStatus.SELECTED, id: 1 },
+      { row: 1, seat: 2, status: SeatStatus.SELECTED, id: 2 },
+    ];
+
+    fixture.detectChanges();
+
+    const seatsElement = fixture.debugElement.queryAll(By.css('.selected-seats-info'));
+    const rowElements = fixture.debugElement.queryAll(By.css('[data-test="seat-row"]'));
+    const seatNumberElements = fixture.debugElement.queryAll(By.css('[data-test="seat-seat"]'));
+
+
+    expect(seatsElement.length).toBe(component.selectedSeatsList.length);
+    expect(Number(rowElements[0].nativeElement.textContent)).toBe(component.selectedSeatsList[0].row);
+    expect(Number(rowElements[1].nativeElement.textContent)).toBe(component.selectedSeatsList[1].row);
+    expect(Number(seatNumberElements[1].nativeElement.textContent)).toBe(component.selectedSeatsList[1].seat);
+    expect(Number(seatNumberElements[1].nativeElement.textContent)).toBe(component.selectedSeatsList[1].seat);
+
   });
 });
--- BePalmWatchFE/src/app/shared/ui/selected-seats/selected-seats.component.ts
+++ BePalmWatchFE/src/app/shared/ui/selected-seats/selected-seats.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Seat } from '../../../core/models/seat.model';
 import { CommonModule } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-selected-seats',
