137b84b2630ef49f105ff5d534b066d6938de5a0
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-22 18:18:10 +0200

    add test for seat legend component

--- BePalmWatchFE/src/app/shared/ui/seat-legend/seat-legend.component.spec.ts
+++ BePalmWatchFE/src/app/shared/ui/seat-legend/seat-legend.component.spec.ts
@@ -20,4 +20,20 @@ describe('SeatLegendComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct number of seat for the legend', () => {
+
+    const seatElement = fixture.nativeElement.querySelectorAll('app-seat');
+
+    expect(seatElement.length).toBe(component.LEGEND_SEAT.length);
+
+  });
+
+  it('should set the correct label', () => {
+    const labelElement = fixture.nativeElement.querySelectorAll('.legend-container p');
+
+    expect(labelElement[0].textContent).toBe(component.LEGEND_SEAT[0].label);
+    expect(labelElement[1].textContent).toBe(component.LEGEND_SEAT[1].label);
+    expect(labelElement[2].textContent).toBe(component.LEGEND_SEAT[2].label);
+  })
 });
