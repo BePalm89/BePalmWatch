a26f2f81880a88bef15fac0fb9aad1e3282e0d40
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-22 17:48:12 +0200

    add tests for the hour selector component

--- BePalmWatchFE/src/app/shared/ui/hour-selector/hour-selector.component.spec.ts
+++ BePalmWatchFE/src/app/shared/ui/hour-selector/hour-selector.component.spec.ts
@@ -1,23 +1,70 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HourSelectorComponent } from './hour-selector.component';
+import { HourSelectorComponent } from "./hour-selector.component";
+import { ShowtimeService } from "../../../core/services/showtime.service";
+import { By } from "@angular/platform-browser";
 
-describe('HourSelectorComponent', () => {
+describe("HourSelectorComponent", () => {
   let component: HourSelectorComponent;
   let fixture: ComponentFixture<HourSelectorComponent>;
+  let showtimeService: ShowtimeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HourSelectorComponent]
-    })
-    .compileComponents();
-    
+      imports: [HourSelectorComponent],
+    }).compileComponents();
+
+    showtimeService = TestBed.inject(ShowtimeService);
     fixture = TestBed.createComponent(HourSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should display the correct number of hours", () => {
+    const divElement = fixture.nativeElement.querySelectorAll(
+      ".hours-container div"
+    );
+
+    expect(divElement.length).toBe(component.HOURS.length);
+  });
+
+  it("should set the correct hour when select a particular hour and call the showtime service", () => {
+    // Given
+    jest.spyOn(showtimeService, "updateTime").mockImplementation(jest.fn());
+
+    // When
+    component.onSelectHour("20:00");
+
+    fixture.detectChanges();
+
+    expect(component.selectedHour).toBe("20:00");
+    expect(showtimeService.updateTime).toHaveBeenCalledWith("20:00");
+  });
+
+  it("should set the correct hour and the right style when an hour is selected", () => {
+    // Given
+    const hourToSelect = "22:00";
+
+    // When
+
+    component.onSelectHour(hourToSelect);
+    fixture.detectChanges();
+
+    const hourElement = fixture.debugElement.queryAll(
+      By.css(".hours-container div")
+    );
+
+    const selectedElement = hourElement.find((el) => {
+      return el.nativeElement.textContent.trim() === hourToSelect;
+    });
+
+    // Then
+    expect(selectedElement).toBeTruthy();
+    expect(component.selectedHour).toBe("22:00");
+    expect(selectedElement?.nativeElement.classList).toContain('selected');
   });
 });
