7dade823169776bda78ed357dcbce9afbc7794d5
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-25 17:49:05 +0200

    add test for home component

--- BePalmWatchFE/src/app/features/home/home.component.spec.ts
+++ BePalmWatchFE/src/app/features/home/home.component.spec.ts
@@ -1,23 +1,63 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HomeComponent } from './home.component';
+import { HomeComponent } from "./home.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { MoviesService } from "../../core/services/movies.service";
+import { GenreService } from "../../core/services/genre.service";
+import { of } from "rxjs";
+import { provideRouter } from "@angular/router";
 
-describe('HomeComponent', () => {
+describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let genreService: GenreService;
+  let movieService: MoviesService;
+
+  const mockMovies = {
+    results: [
+      { id: 1, title: "Movie1" },
+      { id: 2, title: "Movie2" },
+    ],
+  };
+  const mockGenres = [
+    { id: 1, name: "Action" },
+    { id: 2, name: "Drama" },
+  ];
 
   beforeEach(async () => {
+    const movieServiceMock = {
+      getMovies: jest.fn().mockReturnValue(of(mockMovies)),
+    };
+
+    const genreServiceMock = {
+      getGenreMovie: jest.fn().mockReturnValue(mockGenres),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomeComponent, HttpClientTestingModule],
+      providers: [
+        { provide: MoviesService, useValue: movieServiceMock },
+        { provide: GenreService, useValue: genreServiceMock },
+        provideRouter([])
+      ],
+      
+    }).compileComponents();
+
+    movieService = TestBed.inject(MoviesService);
+    genreService = TestBed.inject(GenreService);
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getMovies and getGenreMovie on init', () => {
+
+    expect(genreService.getGenreMovie).toHaveBeenCalled();
+    expect(movieService.getMovies).toHaveBeenCalled();
+
   });
 });
