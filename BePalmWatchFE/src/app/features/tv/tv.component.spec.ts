3a340e1cf627e0e6da046824293faf57a004867c
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-25 17:29:45 +0200

    add test for tv component

--- BePalmWatchFE/src/app/features/tv/tv.component.spec.ts
+++ BePalmWatchFE/src/app/features/tv/tv.component.spec.ts
@@ -1,17 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TvComponent } from './tv.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TvService } from '../../core/services/tv.service';
+import { of } from 'rxjs';
+import { GenreService } from '../../core/services/genre.service';
+import { provideRouter } from '@angular/router';
 
 describe('TvComponent', () => {
   let component: TvComponent;
   let fixture: ComponentFixture<TvComponent>;
+  let tvService: TvService;
+  let genreService: GenreService;
+
+  const mockTvShows = { results: [{ id: 1, name: 'Show1' }, { id: 2, name: 'Show2' }] };
+  const mockGenres = [{ id: 1, name: 'Action' }, { id: 2, name: 'Drama' }];
 
   beforeEach(async () => {
+
+    const tvServiceMock = {
+      getTVShows: jest.fn().mockReturnValue(of(mockTvShows))
+    };
+
+    const genreServiceMock = {
+      getGenreTvShow: jest.fn().mockReturnValue(mockGenres)
+    };
+
+
     await TestBed.configureTestingModule({
-      imports: [TvComponent]
+      imports: [TvComponent, HttpClientTestingModule],
+      providers: [
+        { provide: TvService, useValue: tvServiceMock },
+        { provide: GenreService, useValue: genreServiceMock },
+        provideRouter([])
+      ],
     })
     .compileComponents();
     
+    tvService = TestBed.inject(TvService);
+    genreService = TestBed.inject(GenreService);
     fixture = TestBed.createComponent(TvComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +46,13 @@ describe('TvComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should call getTVShows and getGenreTvShow on init and set tvShows', () => {
+
+    expect(genreService.getGenreTvShow).toHaveBeenCalled();
+    expect(tvService.getTVShows).toHaveBeenCalled();
+
   });
 });
