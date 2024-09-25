c17c73f0f266a99f8cfa64efb3eb8c5824f2623a
BePalm89 <41806213+BePalm89@users.noreply.github.com>
2024-09-25 17:41:16 +0200

    add test for now playing component

--- BePalmWatchFE/src/app/features/now-playing/now-playing.component.spec.ts
+++ BePalmWatchFE/src/app/features/now-playing/now-playing.component.spec.ts
@@ -1,23 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { NowPlayingComponent } from './now-playing.component';
+import { NowPlayingComponent } from "./now-playing.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { NowPlayingService } from "../../core/services/now-playing.service";
+import { of } from "rxjs";
+import { provideRouter } from "@angular/router";
+import dayjs from "dayjs";
 
-describe('NowPlayingComponent', () => {
+describe("NowPlayingComponent", () => {
   let component: NowPlayingComponent;
   let fixture: ComponentFixture<NowPlayingComponent>;
+  let nowPlayingService: NowPlayingService;
+
+  const mockNowPlayingMovies = [{ id: 1, title: "Show1" }];
 
   beforeEach(async () => {
+    const nowPlayingServiceMock = {
+      getNowPlayingMovies: jest.fn().mockReturnValue(of(mockNowPlayingMovies)),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [NowPlayingComponent]
-    })
-    .compileComponents();
-    
+      imports: [NowPlayingComponent, HttpClientTestingModule],
+      providers: [
+        { provide: NowPlayingService, useValue: nowPlayingServiceMock },
+        provideRouter([]),
+      ],
+    }).compileComponents();
+
+    nowPlayingService = TestBed.inject(NowPlayingService);
     fixture = TestBed.createComponent(NowPlayingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should call getNowPlayingMovies when loading the page", () => {
+    expect(nowPlayingService.getNowPlayingMovies).toHaveBeenCalled();
+  });
+
+  it('should se the correct date when loading the component', () => {
+    const today = dayjs().toString();
+    const futureDay = dayjs().add(4, 'day').toString();
+
+    expect(component.date?.minimum).toBe(today);
+    expect(component.date?.maximum).toBe(futureDay);
+  })
 });
--- BePalmWatchFE/src/app/features/now-playing/now-playing.component.ts
+++ BePalmWatchFE/src/app/features/now-playing/now-playing.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { NowPlayingService } from '../../core/services/now-playing.service';
 import { CardContainerComponent } from '../../shared/ui/card-container/card-container.component';
-import { GenreService } from '../../core/services/genre.service';
 import { Date } from '../../core/models/date.model';
 import { CommonModule } from '@angular/common';
 import dayjs from 'dayjs';
@@ -17,15 +16,14 @@ import { MediaTypeEnum } from '../../core/enum/media-type.enum';
 export class NowPlayingComponent {
 
   private readonly nowPlayingService = inject(NowPlayingService);
-  private readonly genreService = inject(GenreService);
 
   nowPlayingMovies: any[] = [];
   date: Date | null = null;
   mediaType = MediaTypeEnum;
 
   constructor() {
+    this.date = { minimum: dayjs().toString(), maximum: dayjs().add(4, 'day').toString() }
     this.nowPlayingService.getNowPlayingMovies().subscribe((resp) => {
-        this.date = { minimum: dayjs().toString(), maximum: dayjs().add(4, 'day').toString() }
         this.nowPlayingMovies = resp;
     })
   }
